7919c0834790ef3fd47aef8aed6c5b7b8b1cf54a
Gopi Krishna <gopikrishna0132@gmail.com>
2020-10-26 01:54:39 +0530

    Fix for table's creation issue

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,17 @@
 const { Sequelize } = require("sequelize");
 const { SEQUELIZE_OPTIONS } = require("../config");
-require("../models/user");
-require("../models/chat_room");
-require("../models/message");
+const User = require("../models/user");
+const ChatRoom = require("../models/chat_room");
+const Message = require("../models/message");
 
 const sequelize = new Sequelize(SEQUELIZE_OPTIONS);
 
 return sequelize
     .authenticate()
     .then(async (result) => {
-        await sequelize.sync();
+        await User.sync();
+        await ChatRoom.sync();
+        await Message.sync();
         console.log("Db Initialized!");
     })
     .catch((error) => {
